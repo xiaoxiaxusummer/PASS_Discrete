203bee7989fd42ea5dddcdb1553e84b503e47c20
Xiaoxia Xu <112537016+xiaoxiaxusummer@users.noreply.github.com>
2025-05-01 00:38:31 +0100

    Delete utils_SM directory

--- utils_SM/preference_list.m
+++
@@ -1,6 +0,0 @@
-function WP_list = preference_list(L, N, K, H_herm, G, G_diag, sigma2, gamma_min, A_new)
-    [status, P_new, D, W] = optimize_D_matching(L, N, K, H_herm, G, G_diag, A_new, gamma_min, Pmax, sigma2);
-
-
-end
-
